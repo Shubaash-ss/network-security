056ef0c7cb2afb8c22bfb61e7b79a28c749e025b
Shubaash-ss <sssubash368@gmail.com>
2026-08-18 09:12:58 +0530

    Fix push_data.py bugs and add dependencies

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
-from pymongo.mongo_client import MongoClient
+from pymongo import MongoClient
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://sssubash368_db_user:Admin123@cluster0.lron3bo.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
